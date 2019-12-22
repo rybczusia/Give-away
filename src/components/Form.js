069ed5a10b71ab1cab7e8bdb456a301403e3cb6c
rybczusia <katarzyna.rybka.priv@gmail.com>
2019-12-22 13:30:02 +0100

    Form validation work

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-
-
 class Form extends React.Component {
     constructor(props) {
         super(props);
@@ -12,53 +10,56 @@ class Form extends React.Component {
             errors: {
                 name: false,
                 email: false,
-                message: false,
+                message: false
             }
-
         };
     }
     messages = {
         name_incorrect: 'Podane imię jest nieprawidłowe!',
         email_incorrect: 'Podany mail jest nieprawidłowy!',
-        message_incorrect: 'Wiadomość musi mieć minimm 120 znaków!',
+        message_incorrect: 'Wiadomość musi mieć minimm 120 znaków!'
     };
-handleChange = (e) => {
-    const value = e.currentTarget.value;
-    const name = e.target.name;
+    handleChange = (e) => {
+        const value = e.currentTarget.value;
+        const name = e.target.name;
 
-    this.setState({
-        [name]: value
-    })
-};
-formValidation = () => {
-    let name = false;
-    let email = false;
-    let message = false;
-    let correct = false;
+        this.setState({
+            [name]: value
+        });
+    };
+    formValidation() {
+        let name = false;
+        let email = false;
+        let message = false;
+        let correct = false;
 
-    if (this.state.name.length > 3 && this.state.name.indexOf('') === -1) {
-        name = true;
-    }
-    if (this.state.email.indexOf('@') !== -1) {
-        email = true;
-    }
-    if (this.state.message.length > 119 && this.state.name.indexOf('') === -1) {
-        message = true;
-    }
-    if (name && email && message) {
-        correct = true;
-    }
-    return ({
-        name,
-        email,
-        message,
-        correct
-    })
-};
+        if (this.state.name.length > 3 && this.state.name.indexOf(' ') === -1) {
+            name = true;
+        }
+        if (this.state.email.indexOf('@') !== -1) {
+            email = true;
+        }
+        if (this.state.message.length > 5) {
+            message = true;
+        }
+        if (name && email && message) {
+            correct = true;
+        }
+        console.log('valid' + name);
+        console.log('valid' + email);
+        console.log('valid' + message);
 
+        console.log('valid' + correct);
+        return ({
+            name,
+            email,
+            message,
+            correct
+        });
+    };
     handleSubmit = (e) => {
         e.preventDefault();
-        const validation = this.formValidation()
+        const validation = this.formValidation();
         if (validation.correct) {
             this.setState({
                 name: '',
@@ -67,49 +68,50 @@ formValidation = () => {
                 errors: {
                     name: false,
                     email: false,
-                    message: false,
+                    message: false
                 }
-            })
+            });
         } else {
             this.setState({
-                name: !validation.name,
-                email: !validation.mail,
-                message: !validation.message,
-            })
-
+                errors: {
+                    name: !validation.name,
+                    email: !validation.email,
+                    message: !validation.message
+                }
+            });
         }
     };
-
-
     render() {
 
+        return (
+            <form className='home-footer-form' onSubmit={this.handleSubmit}>
+                <div className='home-form-line'>
+                    <label>
+                        Wpisz swoje imię:
+                        <input type="text" value={this.state.name} name='name' onChange={this.handleChange}/>
+                        {this.state.errors.name && <span className='form-error'>{this.messages.name_incorrect}</span>}
+                    </label>
+                    <label>
+                        Wpisz swój e-mail:
+                        <input type="mail" value={this.state.email} name='email' onChange={this.handleChange}/>
+                        {this.state.errors.email && <span className='form-error'>{this.messages.email_incorrect}</span>}
+                    </label>
+                </div>
+                <div className='home-form-textarea'>
+                    <label>
+                        Wpisz wiadomość:
+                        <textarea value={this.state.message} name='message' onChange={this.handleChange}/>
+                        {this.state.errors.message &&
+                        <span className='form-error'>{this.messages.message_incorrect}</span>}
+                    </label>
+                </div>
 
-    return (
-        <form className='home-footer-form' onSubmit={this.handleSubmit}>
-            <div className='home-form-line'>
-                <label>
-                    Wpisz swoje imię:
-                    <input type="text" value={this.state.name} name='name'onChange={this.handleChange}/>
-                    {this.state.errors.name && <span>{this.messages.name_incorrect}</span>}
-                </label>
-                <label>
-                    Wpisz swój e-mail:
-                    <input type="mail" value={this.state.email} name='email' onChange={this.handleChange}/>
-                    {this.state.errors.email && <span>{this.messages.email_incorrect}</span>}
-                </label>
-            </div>
-            <div className='home-form-textarea'>
-                <label>
-                    Wpisz wiadomość:
-                    <textarea value={this.state.message} name='message'/>
-                    {this.state.errors.message && <span>{this.messages.message_incorrect}</span>}
-                </label>
-            </div>
+                <input className='home-footer-submit' name='message' type="submit" value="Wyślij"
+                       onClick={this.handleSubmit}/>
 
-            <input className='home-footer-submit' name='message' type="submit" value="Wyślij"/>
-
-        </form>
-    );
+            </form>
+        );
+    }
 }
-}
+
 export default Form;
