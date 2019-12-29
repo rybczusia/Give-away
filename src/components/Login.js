0144457afcffe1f933ae0e644d3fbed4a86f3aff
rybczusia <katarzyna.rybka.priv@gmail.com>
2019-12-29 19:36:09 +0100

    login validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { HomeLogin } from './Home';
-import { HomeNavigation } from './Home';
+import {HomeLogin} from './Home';
+import {HomeNavigation} from './Home';
 
 
-
-class Login extends React.Component{
+class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -14,37 +13,129 @@ class Login extends React.Component{
                 email: false,
                 message: false
             }
-        }
+        };
     }
+
     messages = {
         pass_incorrect: 'Podane hasło jest nieprawidłowe!',
-        email_incorrect: 'Podany mail jest nieprawidłowy!',
+        email_incorrect: 'Podany mail jest nieprawidłowy!'
     };
+    handleChange = (e) => {
+        const value = e.currentTarget.value;
+        const name = e.target.name;
+
+        this.setState({
+            [name]: value
+        });
+    };
+
+    formValidation() {
+        let email = false;
+        let pass = false;
+        let correct = false;
+
+        if (this.state.email.indexOf('@') !== -1) {
+            email = true;
+        }
+        if (this.state.pass.length > 5) {
+            pass = true;
+        }
+        if (email && email) {
+            correct = true;
+        }
+        console.log('valid' + pass);
+        console.log('valid' + email);
+        console.log('valid' + correct);
+        return ({
+            pass,
+            email,
+            correct
+        });
+    };
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const validation = this.formValidation();
+        if (validation.correct) {
+            fetch('https://fer-api.coderslab.pl/v1/portfolio/contact', {
+                method: 'POST',
+                body: JSON.stringify({
+                    email: this.state.email,
+                    pass: this.state.message
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then((resp) => resp.json())
+                .then((resp => {
+                    console.log(resp);
+                    if (resp.status === 'success') {
+                        return (alert('Zalogowano'))
+                    }
+                }))
+                .catch(error => {
+                    if (error.status) {
+                        return (alert('Błąd'))
+                    }
+                });
+
+            this.setState({
+                email: '',
+                pass: '',
+                errors: {
+                    email: false,
+                    pass: false
+                }
+            });
+
+        } else {
+            this.setState({
+                errors: {
+                    email: !validation.email,
+                    pass: !validation.message
+                }
+            });
+        }
+    };
+
+
     render() {
         return (
             <>
-                <HomeLogin/>
-                <HomeNavigation/>
-                <div className="login">
-                    <h2 className='home-headline'>Zaloguj się</h2>
-                    <img className='home-icon' src={require('../assets/assets/Decoration.svg')} alt=''/>
-                </div>
-                <div className='login-form'>
-                    <label>
-                        Email:
-                        <input type="mail" value={this.state.name} name='email' onChange={this.handleChange}/>
-                        {this.state.errors.name && <span className='form-error'>{this.messages.email_incorrect}</span>}
-                    </label>
-                    <label>
-                        Hasło:
-                        <input type="mail" value={this.state.email} name='pass' onChange={this.handleChange}/>
-                        {this.state.errors.email && <span className='form-error'>{this.messages.pass_incorrect}</span>}
-                    </label>
+                <div className='login-wrapper'>
+                    <HomeLogin/>
+                    <HomeNavigation/>
+                    <form className="login">
+                        <h2 className='home-headline'>Zaloguj się</h2>
+                        <img className='home-icon' src={require('../assets/assets/Decoration.svg')} alt=''/>
+                    </form>
+                    <form className='login-form'>
+                        <div className='login-inputs'>
+                            <label>
+                                Email:
+                                <input type="mail" value={this.state.email} name='email' onChange={this.handleChange}/>
+                                {this.state.errors.email &&
+                                <span className='form-error'>{this.messages.email_incorrect}</span>}
+                            </label>
+                            <label>
+                                Hasło:
+                                <input type="password" value={this.state.pass} name='pass' onChange={this.handleChange}/>
+                                {this.state.errors.email &&
+                                <span className='form-error'>{this.messages.pass_incorrect}</span>}
+                            </label>
+                        </div>
+
+                        <div className='login-buttons'>
+                            <input className='login-button' name='message' type="submit" value="Utwórz konto" onClick={this.handleSubmit}/>
+                            <input className='login-button' name='message' type="submit" value="Zaloguj się" onClick={this.handleSubmit}/>
+                        </div>
+                    </form>
+
                 </div>
             </>
         );
     }
-
-
 }
+
+
 export default Login;
